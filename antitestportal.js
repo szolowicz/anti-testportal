8f95d7554878f8cd859143220275f6feacd4e280
Lumm1t <lummit1337@gmail.com>
2020-08-09 12:19:36 +0200

    Change tab width

--- antitestportal.js
+++ antitestportal.js
@@ -1,21 +1,21 @@
 const antiTestportalScript = document.createElement('script');
 const securityBypass = document.createTextNode(`
-    // Don't display warning about leaving the page.
-    window['honestRespondentBlockade_popup'] = () => {};
-    window.honestRespondentBlockade_popup.open = () => {};
-    window['honestRespondentWarning_popup'] = () => {};
-    window.honestRespondentWarning_popup.open = () => {};
+  // Don't display warning about leaving the page.
+  window['honestRespondentBlockade_popup'] = () => {};
+  window.honestRespondentBlockade_popup.open = () => {};
+  window['honestRespondentWarning_popup'] = () => {};
+  window.honestRespondentWarning_popup.open = () => {};
 
-    // Don't send number of page changes to test creator.
-    updateCt = () => {};
+  // Don't send number of page changes to test creator.
+  updateCt = () => {};
 
-    // If we changed page, remove blurs count.
-    window.onblur = function() {
-        eraseCookie('blurs');
-    }
+  // If we changed page, remove blurs count.
+  window.onblur = function() {
+    eraseCookie('blurs');
+  }
 
-    // If time has elapsed, don't do anything.
-    onCountdownFinished = () => {};
+  // If time has elapsed, don't do anything.
+  onCountdownFinished = () => {};
 `);
 
 antiTestportalScript.appendChild(securityBypass);
