f86277be7b0613347262a99391fb9ecfb7096d55
gbaranski <gbaranski@gbaranskis-MacBook-Pro.local>
2020-11-13 14:22:37 +0100

    fix: remove setting unlimited time
    https://github.com/Lumm1t/anti-testportal/issues/12

--- src/bypass.ts
+++ src/bypass.ts
@@ -8,7 +8,4 @@ export default () => {
 
   // Lumm1t is not defined, ignore all errors.
   window.onerror = (): boolean => true;
-
-  // Set (almost) unlimited time for each question.
-  startTime = Date.parse('January 1, 2070 00:00:00 UTC') as number;
 };
