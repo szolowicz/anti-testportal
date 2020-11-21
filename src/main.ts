325d9cc76f169dd1302f01fd68053f1992d2e301
Lumm1t <lummit1337@gmail.com>
2020-11-21 12:06:03 +0100

    style: add tools

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,32 @@
-import Bypass from './bypass';
+import Bypass from './bypass'
 
 class Solver {
-  protected readonly bypass = new Bypass();
+  protected readonly bypass = new Bypass()
 
-  constructor() {
-    console.clear();
+  constructor () {
+    console.clear()
     console.log(
       '%canti-testportal by Lumm1t\n',
       'background: #222; color: #f00; font-size: 20px;',
       'https://github.com/Lumm1t/anti-testportal'
-    );
+    )
 
     try {
-      this.createScript();
+      this.createScript()
     } catch (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
   }
 
-  private createScript(): void {
-    const script = document.createElement('script') as HTMLScriptElement;
+  private createScript (): void {
+    const script = document.createElement('script') as HTMLScriptElement
 
-    script.setAttribute('type', 'text/javascript');
-    script.innerHTML = `(${this.bypass.createBypass})();`;
+    script.setAttribute('type', 'text/javascript')
+    script.innerHTML = `(${this.bypass.createBypass})();`
 
-    document.body.appendChild<HTMLScriptElement>(script);
+    document.body.appendChild<HTMLScriptElement>(script)
   }
 }
 
-export default new Solver();
+export default new Solver()
