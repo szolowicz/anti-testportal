df50bba3fc0ef7d50373e765d60aba8e0de9cb6d
Lumm1t <lummit1337@gmail.com>
2020-11-21 12:15:04 +0100

    refactor(src): semicolons

--- src/bypass.ts
+++ src/bypass.ts
@@ -1,13 +1,13 @@
-declare const Lumm1t: undefined
+declare const Lumm1t: undefined;
 
 export default class Bypass {
   public createBypass (): void {
     // Remove ability to read focus value.
     Object.defineProperty(document, 'hasFocus', {
       get: (): ReferenceError => Lumm1t
-    })
+    });
 
     // Lumm1t is not defined, ignore all errors.
-    window.onerror = (): boolean => true
+    window.onerror = (): boolean => true;
   }
 }
