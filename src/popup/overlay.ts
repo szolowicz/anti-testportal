052fd9bbbf953471012eb8bd8568586e04ef80e9
Lumm1t <lummit1337@gmail.com>
2020-11-21 12:06:03 +0100

    style: add tools

--- src/popup/overlay.ts
+++ src/popup/overlay.ts
@@ -1,22 +1,22 @@
 class Overlay {
-  constructor() {
-    this.checkVersion();
+  constructor () {
+    this.checkVersion()
   }
 
-  private checkVersion(): void {
-    const update = document.getElementById('update');
+  private checkVersion (): void {
+    const update = document.getElementById('update')
 
-    const manifestVersion = chrome.runtime.getManifest().version;
-    const url = 'https://raw.githubusercontent.com/Lumm1t/anti-testportal/master/dist/manifest.json';
+    const manifestVersion = chrome.runtime.getManifest().version
+    const url = 'https://raw.githubusercontent.com/Lumm1t/anti-testportal/master/dist/manifest.json'
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
       update.innerText = manifestVersion === data.version ?
         `up to date (${manifestVersion})` :
-        `update available (${data.version})`;
-    });
+        `update available (${data.version})`
+    })
   }
 }
 
-export default new Overlay();
+export default new Overlay()
