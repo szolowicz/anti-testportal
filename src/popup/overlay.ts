d2dfc9011534f0de8e99344953e3299e68aa4c81
gbaranski <gbaranski19@gmail.com>
2020-11-22 23:52:06 +0100

    style: reformatted whole code

--- src/popup/overlay.ts
+++ src/popup/overlay.ts
@@ -1,20 +1,22 @@
 class Overlay {
-  constructor () {
+  constructor() {
     this.checkVersion();
   }
 
-  private checkVersion (): void {
+  private checkVersion(): void {
     const update = document.getElementById('update');
 
     const manifestVersion = chrome.runtime.getManifest().version;
-    const url = 'https://raw.githubusercontent.com/Lumm1t/anti-testportal/master/dist/manifest.json';
+    const url =
+      'https://raw.githubusercontent.com/Lumm1t/anti-testportal/master/dist/manifest.json';
 
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        update.innerText = manifestVersion === data.version ?
-          `up to date (${manifestVersion})` :
-          `update available (${data.version})`;
+      .then((response) => response.json())
+      .then((data) => {
+        update.innerText =
+          manifestVersion === data.version
+            ? `up to date (${manifestVersion})`
+            : `update available (${data.version})`;
       });
   }
 }
