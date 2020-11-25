2752da0861d5aa32aee98f5311a7deeac1061caa
Lumm1t <lummit1337@gmail.com>
2020-11-25 02:06:00 +0100

    feat(discordinvite): run only on install

--- public/discordinvite.js
+++ public/discordinvite.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
-chrome.runtime.onInstalled.addListener((object) => {
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason !== "install") return;
+
   chrome.tabs.create({
     url: 'https://discord.gg/HPecVXeQrF',
     active: true
